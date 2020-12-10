597a1e70d7458a5b48fe23a3ead2b5e3338c8170
thao <1851161594@e.tlu.edu.vn>
2020-12-10 21:22:05 +0700

    First

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  resources :users
+  get '/users/new', to: 'users#new'
+  get '/users/:id', to:'users#show'
+  get '/users', to: 'users#index'
+  post '/users', to: 'users#create'
 end
